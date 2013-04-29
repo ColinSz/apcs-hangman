cdf304788b183f5f82ea0415d5bba856b6b273d6
ColinSz <kenavt@gmail.com>
2013-04-29 13:17:00 -0400

    Game appears to be finished! Will be shipping later today.

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -13,7 +13,7 @@ import java.util.Random;
  */
 public class Hangman extends BasicGame {
     
-    private char[] lettersActual = {'_', '_', '_', '_', '_',};
+    private char[] lettersActual = {'_', '_', '_', '_', '_'};
     private boolean[] letters = {false, false, false, false, false};
     private String alphabet = "abcdefghijklmnopqrstuvwxyz";
     private String choseWord = "rhymeloirelunarmailsbasisfilchwieldvisitleadsbrats"
@@ -26,7 +26,7 @@ public class Hangman extends BasicGame {
     
     
     public Hangman(){ 
-        reinitializeGame();
+        //reinitializeGame();
         JOptionPane.showMessageDialog(null, "Welcome to Hangman! Are you ready to play?");
     }
     
@@ -40,6 +40,7 @@ public class Hangman extends BasicGame {
     
     @Override
      public void playOneGame(){    
+          reinitializeGame();
           while (shouldContinue()) {    
                updateStatus(askUsersToGuess());
           }
@@ -48,13 +49,17 @@ public class Hangman extends BasicGame {
 
     public void reinitializeGame(){
         Random myRandom = new Random();
-        int letterStart = myRandom.nextInt(5)*50;
+        int letterStart = myRandom.nextInt(50)*5;
         System.out.println("" + letterStart);
         word = choseWord.substring(letterStart, letterStart+5);
-        //word = choseWord.substring(100, 105);
-        System.out.println(word);
+        System.out.println("reinitializing game");
         for(boolean change: letters)
             change = false;
+        lettersActual[0] = '_';
+        lettersActual[1] = '_';
+        lettersActual[2] = '_';
+        lettersActual[3] = '_';
+        lettersActual[4] = '_';
         guessesLeft = 5;
         alphabet = "abcdefghijklmnopqrstuvwxyz";
     }
@@ -71,18 +76,18 @@ public class Hangman extends BasicGame {
                         
              }
              catch(NullPointerException e){
-                JOptionPane.showMessageDialog(null, "Put in something real!");
-                letterGuessed = '.';
+               System.exit(0);
              }
              catch(StringIndexOutOfBoundsException e){
-                JOptionPane.showMessageDialog(null, "Put in something real!");
-                letterGuessed = '.';
+                System.out.println("threw that indexoutofboundsexception");
+                JOptionPane.showMessageDialog(null, "Put in something real!\nYou have " + guessesLeft + " guesses left!");
+                letterGuessed = ' ';
              }
              System.out.println(letterGuessedValid(letterGuessed) + "");
-             if(letterGuessedValid(letterGuessed))
+             if((letterGuessed != ' ') && letterGuessedValid(letterGuessed))
                  JOptionPane.showMessageDialog(null, "You already used that letter, " + letterGuessed
                          + ". Try one of these: " + alphabet);
-         } while(letterGuessedValid(letterGuessed));
+         } while((letterGuessed == ' ') && letterGuessedValid(letterGuessed));
          
          System.out.println("Exited guessing loop");
          
@@ -95,71 +100,51 @@ public class Hangman extends BasicGame {
      
      public void updateStatus(char letterGuessed){
          int index = 0;
+         System.out.println("letter         " + letterGuessed);
          while(index != -1) {
             index = alphabet.indexOf(String.valueOf(letterGuessed));
-            System.out.println(index);
+            //System.out.println(index);
             if(index != -1){
                 alphabet = alphabet.substring(0, index) + alphabet.substring(index+1, alphabet.length());
                 System.out.println(alphabet);
             }
             if((word.indexOf(String.valueOf(letterGuessed))) != -1){
                 String restOfWord = word;
-                String restOfWordMissing = word;
+                String restOfWordMissing = "";
                 int spot = 0;
-                while(spot < restOfWord.length())
-                    restOfWord = word.substring(spot, word.length());
-                    restOfWordMissing = word.substring(0, spot);
-                    System.out.println(spot);
-                    System.out.println(restOfWord);
-                    System.out.println(restOfWordMissing);
-                    spot = restOfWord.indexOf(String.valueOf(letterGuessed));
+                while(spot < restOfWord.length()) {
+                    
+                    spot = restOfWord.indexOf(String.valueOf(letterGuessed)); //+ restOfWordMissing.length();
+                    if(spot >= 0)
+                       spot += restOfWordMissing.length(); 
+                    //System.out.println("spot                " + spot);
+                    restOfWord = word.substring(spot+1, word.length());
+                    restOfWordMissing = word.substring(0, spot+1);
+                    //System.out.println("restOfWord          " + restOfWord);
+                    //System.out.println("restOfWordMissing   " + restOfWordMissing);
                     if(spot < 0)
-                        spot = restOfWord.length() + 1;
+                        spot = restOfWord.length() + 2;
                     else {
-                        letters[restOfWordMissing.length() + spot] = true;
-                        lettersActual[restOfWordMissing.length() + spot] = word.charAt(spot);
-                    }
-                    System.out.println(spot);
-                }
-                
-/*              String restOfWord = word;
-                int spot = 0;
-                int i = 0;
-                spot = restOfWord.substring(spot, restOfWord.length()).indexOf(String.valueOf(letterGuessed));
-                int spotOld = spot;
-                JOptionPane.showMessageDialog(null, "The letter you guessed, " + letterGuessed + ", is in the word! Nice job!"
-                     + "\nHere's the letters you have left: " + alphabet + ".");
-                do{
-                    spotOld = spot;
-                    if(i > 0)
-                        spot += (restOfWord.substring(spot, restOfWord.length()).indexOf(String.valueOf(letterGuessed))) + 1;
-                    else {
-                        spot += restOfWord.substring(spot, restOfWord.length()).indexOf(String.valueOf(letterGuessed));
-                        System.out.println(spot + " IN loop");
-
                         letters[spot] = true;
                         lettersActual[spot] = word.charAt(spot);
-                        spot++;
                     }
-                    restOfWord = word.substring(spot+1, word.length());
-                    System.out.println(restOfWord + " IN loop");
-                    i++;
-                } while(spot < restOfWord.length());
-                index = -1;
-            }*/
+                    //System.out.println(spot);
+                }
+            }
             else {
-                guessesLeft--;
-                JOptionPane.showMessageDialog(null, "Nope, sorry!\nThe letter you guessed, " + letterGuessed + ", doesn't belong."
-                     + "\nHere's the letters you have left: " + alphabet + "."
-                     + "\nYou have " + guessesLeft + "guesses left! Good luck!");
-                index = -1;
+                if(letterGuessed != ' ') 
+                    guessesLeft--;
+                    JOptionPane.showMessageDialog(null, "Nope, sorry!\nThe letter you guessed, " + letterGuessed + ", doesn't belong."
+                         + "\nHere's the letters you have left: " + alphabet + "."
+                        + "\nYou have " + guessesLeft + " guesses left! Good luck!");
+                    index = -1;
             }
          }
      }
      
      @Override
      public boolean shouldContinue(){
-         return !(letters[0] && letters[1] && letters[2] && letters[3] && letters[4]);
+         return !((guessesLeft == 0)||(letters[0] && letters[1] && letters[2] && letters[3] && letters[4]));
      }  //======================
 
      public void showFinalStatus(boolean success){    
@@ -167,7 +152,7 @@ public class Hangman extends BasicGame {
             JOptionPane.showMessageDialog (null,
                          "That was right.  \nCongratulations! The word was " + word + ".");
          else
-             JOptionPane.showMessageDialog(null, "You ran out of guesses! The word was " + word + ". ");
+             JOptionPane.showMessageDialog(null, "You ran out of guesses! The word was " + word + ".");
      }  //======================
     
 }
